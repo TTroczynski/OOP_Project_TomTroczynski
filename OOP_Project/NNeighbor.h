1fd10b6c97c7729ded77670cd823e0e5f19be03d
Tom Troczynski <Ttroczynski3063@conestogac.on.ca>
2022-04-11 18:31:52 -0400

    Commentary tweaked

--- OOP_Project/NNeighbor.h
+++ OOP_Project/NNeighbor.h
@@ -2,8 +2,11 @@
 //! NNeighbor Class
 /*!
 * A machine learning model that inherits from the Classifier class.
-* This model makes is trained using Data objects containing x, y, z coordinates and a classifier label.
+* This model is trained using Data objects containing x, y, z coordinates and a classifier label.
 * Predicitions are made first by checking for an edge case, 2 equidistant points,
+* The closest point is selected when classifying a vector. If there are two equidistant points
+* then, the mean location for each set containing each label is evaluated. The closes mean is chosen.
+* 
 * This class implements:
 * @see train()
 * @see predict()
